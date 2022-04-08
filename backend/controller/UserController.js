66467f07517138575c7a90e141680ff1d5235f39
Sajid Hussain <sajidhussain98@gmail.com>
2022-04-09 00:41:40 +0530

    Error and Exceptional Handling

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -1,10 +1,29 @@
-const registerUser=(req,res)=>{
-    res.send('Register Route')
-}
+const asyncHandler=require('express-async-handler')
+// @desc Register a new user
+// @route /api/users
+//@access public
+const registerUser=asyncHandler( async(req,res)=>{
 
-const loginUser = (req, res) => {
+
+  const {name,email,password}=req.body
+  //Validation
+
+  if (!name || !email || !password) {
+
+    res.status(400)
+
+    throw new Error ('Please include all fields')
+  }
+  res.send('Register Route')
+})
+
+
+// @desc Login a user
+// @route /api/users/login
+//@access public
+const loginUser =asyncHandler( async(req, res) => {
   res.send('Login Route');
-};
+})
 
 module.exports={
     registerUser,
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 const express =require('express')
+const { erroHandler } = require('./middleware/errorMiddleware')
 const dotenv=require('dotenv').config()
 const PORT=process.env.PORT || 8000
 const app=express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 app.get('/',(req,res)=>{
 
     res.status(200).json(`message:welcome to support desk api`)
 })
 
+//Routes
 app.use('/api/users',require('./routes/UserRoutes'))
+app.use(erroHandler)
 
 app.listen(PORT,()=>{
         console.log(`sERVER STARTED ON ${PORT}`);
