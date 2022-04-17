5cccc856c0ee66103111b6747acf76e4010d9dc4
Sajid Hussain <sajidhussain98@gmail.com>
2022-04-17 15:02:48 +0530

    logout Service in Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,33 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, reset } from '../features/auth/authSlice';
 
 const Header = () => {
+
+
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  
+
   return (
     <header className="header">
       <div className="logo">
         <Link to="/">Support Desk</Link>
       </div>
       <ul>
-       
-        
-         
+        {user ? (
+          <li>
+            <button className="btn" onClick={onLogout}>
+              <FaSignOutAlt />
+              Logout
+            </button>
+          </li>
+        ) : (
+          <>
             <li>
               <Link to="/login">
                 <FaSignInAlt /> Login
@@ -22,7 +38,8 @@ const Header = () => {
                 <FaUser /> Register
               </Link>
             </li>
-          
+          </>
+        )}
       </ul>
     </header>
   );
